8ffaf1e9ced20c7b613a3733bc402e4f9040c7b1
fabriziopapi <fabrizio.papi@pagopa.it>
2022-04-19 10:53:08 +0200

    (+) add index export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-// TODO: write you code here
+import * as O from "./Option";
+import * as T from "./Task";
+import * as E from "./Either";
+import * as TE from "./TaskEither";
 
-process.stdout.write("Hello world");
+export const option = {
+  debug: O.debug,
+  error: O.error,
+  info: O.info,
+  log: O.log,
+  warn: O.warn
+};
+
+export const task = {
+  debug: T.debug,
+  error: T.error,
+  info: T.info,
+  log: T.log,
+  warn: T.warn
+};
+
+export const either = {
+  debug: E.debug,
+  error: E.error,
+  info: E.info,
+  log: E.log,
+  warn: E.warn
+};
+
+export const taskEither = {
+  debug: TE.debug,
+  error: TE.error,
+  info: TE.info,
+  log: TE.log,
+  warn: TE.warn
+};
+
+export { useWinston, withContext } from "./utils/azure";
